4bcf7f83351bc64ed47c5531cb66ccb20f762dd0
Brendan Smithyman <brendan@bitsmithy.net>
2016-04-29 11:55:24 -0400

    Add experimental support for other OS versions.

--- pyMKL/__init__.py
+++ pyMKL/__init__.py
@@ -5,12 +5,21 @@ from __future__ import division
 from future import standard_library
 standard_library.install_aliases()
 
+import platform
 import numpy as np
 import scipy.sparse as sp
 from ctypes import CDLL, cdll, RTLD_GLOBAL
 from ctypes import POINTER, byref, c_int, c_longlong
 
-path = 'libmkl_intel_lp64.dylib'
+systype = platform.system()
+if systype == 'Darwin':
+    path = 'libmkl_intel_lp64.dylib'
+elif systype == 'Linux':
+    path = 'libmkl_intel_lp64.so'
+elif systype == 'Windows':
+    path = 'mkl_intel_lp64_dll.lib'
+else:
+    raise ImportError('Cannot determine MKL library to import')
 MKLlib = CDLL(path, RTLD_GLOBAL)
 
 from .pardisoInterface import pardisoinit, pardiso
