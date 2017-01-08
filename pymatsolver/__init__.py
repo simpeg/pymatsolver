6966bea6713e95b723a22462173e83154ef9fe39
Rowan Cockett <rowanc1@gmail.com>
2017-01-07 16:28:35 -0800

    Remove mumps

--- pymatsolver/__init__.py
+++ pymatsolver/__init__.py
@@ -21,31 +21,8 @@ AvailableSolvers = {
     "SolverLU": True,
     "SolverCG": True,
     "Triangle": True,
-    "Mumps": False,
-    "Pardiso": False,
+    "Pardiso": False
 }
-
-
-try:
-    from pymatsolver.Mumps import Mumps
-    AvailableSolvers['Mumps'] = True
-except ImportError:
-    SolverHelp['Mumps'] = """Mumps is not working.
-
-
-Ensure that you have Mumps installed, and know where the path to it is.
-
-Try something like:
-
-    cd pymatsolver/Mumps
-    make
-
-When that doesn't work, you may need to edit the make file, to modify the
-path to the mumps installation, or any other compiler flags.
-
-If you find a good way of doing it, please share.
-
-"""
 
 try:
     from pymatsolver.direct import Pardiso
