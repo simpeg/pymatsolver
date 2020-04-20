c07e90540a9cf6815cb934186cffd948a9e53aec
Joseph Capriotti <jcapriot@mines.edu>
2020-04-20 16:44:08 -0600

    rename __init__.py

--- pymatsolver/__init__.py
+++ pymatsolver/__init__.py
@@ -36,25 +36,25 @@ Ensure that you have pyMKL installed, which may also require Python
 to be installed through conda.
 """
 
-try:
-    from pymatsolver.mumps import Mumps
-    AvailableSolvers['Mumps'] = True
-    MumpsSolver = Mumps  # backwards compatibility
-except Exception:
-    SolverHelp['Mumps'] = """Mumps is not working.
-
-Ensure that you have Mumps installed, and know where the path to it is.
-Try something like:
-    cd pymatsolver/mumps
-    make
-
-When that doesn't work, you may need to edit the make file, to modify the
-path to the mumps installation, or any other compiler flags.
-If you find a good way of doing it, please share.
-
-brew install mumps --with-scotch5 --without-mpi
-mpicc --showme
-"""
+# try:
+#     from pymatsolver.mumps import Mumps
+#     AvailableSolvers['Mumps'] = True
+#     MumpsSolver = Mumps  # backwards compatibility
+# except Exception:
+#     SolverHelp['Mumps'] = """Mumps is not working.
+#
+# Ensure that you have Mumps installed, and know where the path to it is.
+# Try something like:
+#     cd pymatsolver/mumps
+#     make
+#
+# When that doesn't work, you may need to edit the make file, to modify the
+# path to the mumps installation, or any other compiler flags.
+# If you find a good way of doing it, please share.
+#
+# brew install mumps --with-scotch5 --without-mpi
+# mpicc --showme
+# """
 
 __version__ = '0.1.2'
 __author__ = 'Rowan Cockett'
