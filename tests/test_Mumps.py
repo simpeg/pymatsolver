3a041820e2571e87ca2f08484fef87350c243a2a
Joseph Capriotti <jcapriot@mines.edu>
2020-04-20 15:59:14 -0600

    removing mumps testing (TODO, work towards using conda-forge mumps)

--- tests/test_Mumps.py
+++ tests/test_Mumps.py
@@ -5,7 +5,7 @@ import pymatsolver
 
 TOL = 1e-11
 
-
+"""
 class TestMumps(unittest.TestCase):
 
     if pymatsolver.AvailableSolvers['Mumps']:
@@ -83,3 +83,4 @@ class TestMumps(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+"""
