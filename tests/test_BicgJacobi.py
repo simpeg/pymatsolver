4efc207912c86166e213a2a8dccc78aaedeef50e
Joseph Capriotti <josephrcapriotti@gmail.com>
2024-10-09 19:10:51 -0600

    run BiCG through the matrix test suite

--- tests/test_BicgJacobi.py
+++ tests/test_BicgJacobi.py
@@ -4,7 +4,7 @@ import numpy.testing as npt
 import scipy.sparse as sp
 import pytest
 
-TOL = 1e-5
+RTOL = 1e-5
 
 @pytest.fixture()
 def test_mat_data():
@@ -15,28 +15,30 @@ def test_mat_data():
     A = A.tocsr()
     np.random.seed(1)
     sol = np.random.rand(nSize, 4)
-    rhs = A.dot(sol)
-    return A, sol, rhs
+    return A, sol
 
 
 @pytest.mark.parametrize('transpose', [True, False])
 @pytest.mark.parametrize('dtype', [np.float64, np.complex128])
-def test_solve(test_mat_data, dtype, transpose):
-    A, rhs, sol = test_mat_data
+@pytest.mark.parametrize('symmetric', [True, False])
+def test_solve(test_mat_data, dtype, transpose, symmetric):
+    A, sol = test_mat_data
     A = A.astype(dtype)
-    rhs = rhs.astype(dtype)
     sol = sol.astype(dtype)
+    if not symmetric:
+        D = sp.diags(np.linspace(2, 3, A.shape[0]))
+        A = D @ A
+    rhs = A @ sol
     if transpose:
-        A = A.T
-        Ainv = BicgJacobi(A).T
+        Ainv = BicgJacobi(A.T, is_symmetric=symmetric).T
     else:
-        Ainv = BicgJacobi(A)
+        Ainv = BicgJacobi(A, is_symmetric=symmetric)
     Ainv.maxiter = 2000
     solb = Ainv * rhs
-    npt.assert_allclose(rhs, A @ solb, atol=TOL)
+    npt.assert_allclose(rhs, A @ solb, rtol=RTOL)
 
 def test_errors_and_warnings(test_mat_data):
-    A, rhs, sol = test_mat_data
+    A, sol = test_mat_data
     with pytest.warns(FutureWarning):
         Ainv = BicgJacobi(A, symmetric=True)
 
@@ -47,7 +49,7 @@ def test_errors_and_warnings(test_mat_data):
         Ainv = BicgJacobi(A, atol=-1.0)
 
 def test_shallow_copy(test_mat_data):
-    A, rhs, sol = test_mat_data
+    A, sol = test_mat_data
     Ainv = BicgJacobi(A, maxiter=100, rtol=1.0E-3, atol=1.0E-16)
 
     attrs = Ainv.get_attributes()
