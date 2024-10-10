a558b37fa6a79fadf7c091de83911ed6f6ec50c0
Joseph Capriotti <josephrcapriotti@gmail.com>
2024-10-10 13:16:04 -0600

    more wrapper tests.

--- tests/test_Wrappers.py
+++ tests/test_Wrappers.py
@@ -1,4 +1,4 @@
-from pymatsolver import SolverCG, SolverLU
+from pymatsolver import SolverCG, SolverLU, wrap_direct, wrap_iterative
 import pytest
 import scipy.sparse as sp
 import warnings
@@ -22,3 +22,50 @@ def test_good_arg_direct():
     with warnings.catch_warnings():
         warnings.simplefilter("error")
         SolverLU(sp.eye(10, format='csc'), permc_spec='NATURAL')
+
+
+def test_bad_direct_function():
+    def bad_direct_func(A):
+        class Empty():
+            def __init__(self, A):
+                self.A = A
+        # this object returned by the function doesn't have a solve method:
+        return Empty(A)
+    WrappedClass = wrap_direct(bad_direct_func, factorize=True)
+
+    with pytest.raises(TypeError, match="instance returned by.*"):
+        WrappedClass(sp.eye(2))
+
+
+
+def test_direct_clean_function():
+    def direct_func(A):
+        class Empty():
+            def __init__(self, A):
+                self.A = A
+
+            def solve(self, x):
+                return x
+
+            def clean(self):
+                self.A = None
+
+        return Empty(A)
+    WrappedClass = wrap_direct(direct_func, factorize=True)
+
+    A = sp.eye(2)
+    Ainv = WrappedClass(A)
+    assert Ainv.A is A
+    assert Ainv.solver.A is A
+    Ainv.clean()
+    assert Ainv.solver.A is None
+
+def test_iterative_deprecations():
+    def iterative_solver(A, x):
+        return x
+
+    with pytest.warns(FutureWarning, match="check_accuracy and accuracy_tol were unused.*"):
+        wrap_iterative(iterative_solver, check_accuracy=True)
+
+    with pytest.warns(FutureWarning, match="check_accuracy and accuracy_tol were unused.*"):
+        wrap_iterative(iterative_solver, accuracy_tol=1E-3)
