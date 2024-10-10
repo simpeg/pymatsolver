df21bbbd8981a836729de70694542d3e694851c3
Joseph Capriotti <josephrcapriotti@gmail.com>
2024-10-10 14:43:10 -0600

    Switch to pyproject.toml based install

--- pymatsolver/__init__.py
+++ pymatsolver/__init__.py
@@ -99,7 +99,22 @@ Ensure that you have python-mumps installed, which may also require Python
 to be installed through conda.
 """
 
-__version__ = '0.2.0'
 __author__ = 'SimPEG Team'
 __license__ = 'MIT'
 __copyright__ = '2013 - 2024, SimPEG Team, https://simpeg.xyz'
+
+from importlib.metadata import version, PackageNotFoundError
+
+# Version
+try:
+    # - Released versions just tags:       0.8.0
+    # - GitHub commits add .dev#+hash:     0.8.1.dev4+g2785721
+    # - Uncommitted changes add timestamp: 0.8.1.dev4+g2785721.d20191022
+    __version__ = version("pymatsolver")
+except PackageNotFoundError:
+    # If it was not installed, then we don't know the version. We could throw a
+    # warning here, but this case *should* be rare. discretize should be
+    # installed properly!
+    from datetime import datetime
+
+    __version__ = "unknown-" + datetime.today().strftime("%Y%m%d")
